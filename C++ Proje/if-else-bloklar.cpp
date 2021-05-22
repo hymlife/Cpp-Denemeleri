e120f6908226fee93d8ed72189d4471e3ac5dbef
Halil İbrahim Dursun <48247076+hymlife@users.noreply.github.com>
2021-05-22 17:46:04 +0300

    Create if-else-bloklar.cpp

---
+++ C++ Proje/if-else-bloklar.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int sayi1;
+int sayi2;
+
+
+cout << "birinci sayiyi giriniz:";
+cin >> sayi1;
+
+cout << "ikinci sayiyi giriniz";
+cin >> sayi2;
+
+if(sayi1> sayi2){
+
+cout << "Ilk Girilen Sayi 2. Sayidan Buyuktur";
+
+}else if (sayi2>sayi1)
+{
+    cout<< "ikinci Girilen Sayi 1. Sayidan Buyuktur";
+}else{
+    cout << "Sayilar Birbirine Esittir";
+}
+
+
+
+
+
+
+}
