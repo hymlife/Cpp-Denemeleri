f2d4dd01d8d7e74ba0c26ceca4aec734ee2a2cd4
Halil İbrahim Dursun <48247076+hymlife@users.noreply.github.com>
2021-05-22 17:19:59 +0300

    Tüm C++ Denemeleri
    Sizler İçin Oluşturulan C++ Denemeleri

---
+++ C++ Proje/Veri-Alarak-Toplama.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int sayi,sayi2;
+
+    cout << "iki adet sayi giriniz:"<<endl;
+    cout <<endl;
+
+    cout <<"birinci sayiyi giriniz:";
+    cin >> sayi;
+
+    cout <<"ikinci sayi giriniz:";
+    cin >> sayi2;
+   
+int toplam = sayi+sayi2;
+    cout<< "Sayilarin Toplami: "<< toplam;
+
+}
+
